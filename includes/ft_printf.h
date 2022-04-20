2f3519f82bf605acfa1f79d1f4866b3375bf8b54
Bohdan Krasnoshchok <bkrasnos@fu-r5-p4.s19.be>
2022-04-20 14:27:14 +0200

    2

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,29 +6,19 @@
 /*   By: bkrasnos <bkrasnos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 11:47:44 by bkrasnos          #+#    #+#             */
-/*   Updated: 2022/04/20 14:19:17 by bkrasnos         ###   ########.fr       */
+/*   Updated: 2022/04/20 14:26:14 by bkrasnos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+
 # include "../libft/libft.h"
 # include <stdarg.h>
 # include <stdlib.h>
 # include <limits.h>
 # include <unistd.h>
-
-int		ft_printf(const char *format);
-void	ft_output_char(t_print *tab, char c, int *a);
-void	ft_output_hexadicimal_capital(t_print *tab, unsigned int nbr, int *a);
-void	ft_output_hexadicimal(t_print *tab, unsigned int nbr, int *a);
-void	ft_output_number_unsigned(t_print *tab, unsigned int nbr, int *a);
-void	ft_output_number(t_print *tab, int nbr, int *a);
-void	ft_output_pointer(t_print *tab, unsigned long long nbr, int *a);
-void	ft_output_string(t_print *tab, char *str, int *a);
-void	ft_putchar(char c, int *a);
-void	ft_putstr(char *str, int *a);
 
 typedef struct s_print
 {
@@ -41,6 +31,18 @@ typedef struct s_print
 	int		sharp;
 	int		width;
 	int		minus;
-}		t_print
+}		t_print;
+
+int		ft_printf(const char *format);
+void	ft_output_char(t_print *tab, char c, int *a);
+void	ft_output_hexadicimal_capital(t_print *tab, unsigned int nbr, int *a);
+void	ft_output_hexadicimal(t_print *tab, unsigned int nbr, int *a);
+void	ft_output_number_unsigned(t_print *tab, unsigned int nbr, int *a);
+void	ft_output_number(t_print *tab, int nbr, int *a);
+void	ft_output_pointer(t_print *tab, unsigned long long nbr, int *a);
+void	ft_output_string(t_print *tab, char *str, int *a);
+void	ft_putchar(char c, int *a);
+void	ft_putstr(char *str, int *a);
+
 
 #endif
